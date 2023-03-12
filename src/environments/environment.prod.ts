f352637112342021eecd0fc631d3f2710933a4c1
Anjani Chilumula <anjanichilumula123@gmail.com>
2023-03-12 13:35:06 +0530

    Weather Applicaition Repo First Commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+import { APP_ID, APP_INITIALIZER } from "@angular/core";
+
+
+export const environment = {
+ production:true,
+ weatherAppUrl :'http://api.weatherstack.com/current',
+ access_key : '9b39154fed1a9b368616b3454ddfc188',
+ query: 'New York'
+
+
+
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production:false,
+  weatherAppUrl :'http://api.weatherstack.com/current',
+  access_key : '9b39154fed1a9b368616b3454ddfc188',
+  query: 'New York'
+}
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
